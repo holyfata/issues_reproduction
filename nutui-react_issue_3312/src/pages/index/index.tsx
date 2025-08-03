34afafc667130fe6fbf3507f495773b393e80c9e
heyongqi10 <heyongqi10@jd.com>
2025-08-03 15:48:32 +0800

    feat: 通过测试问题

--- nutui-react_issue_3312/src/pages/index/index.tsx
+++ nutui-react_issue_3312/src/pages/index/index.tsx
@@ -2,14 +2,19 @@ import { Button, Notify } from '@nutui/nutui-react'
 
 const Demo4 = () => {
   const show1 = () => {
-    Notify.text('123', { duration: 2000 })
+    Notify.text('123', { duration: 5000 })
     setTimeout(() => {
       Notify.hide()
     }, 100)
   }
 
-  const show2 = () => {
-    Notify.text('123', { duration: 2000 })
+  const show2 = async () => {
+    await Notify.text('123', { duration: 5000 })
+    Notify.hide()
+  }
+
+  const show3 = async () => {
+    Notify.text('123', { duration: 5000 })
     Notify.hide()
   }
 
@@ -17,6 +22,7 @@ const Demo4 = () => {
     <>
       <Button onClick={show1}>show1</Button>
       <Button onClick={show2}>show2</Button>
+      <Button onClick={show3}>show3</Button>
     </>
   )
 }
