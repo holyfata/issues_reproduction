f4c24f9756c6f9e6d6e4184ed65a73b22b1b992b
heyongqi10 <heyongqi10@jd.com>
2025-08-03 15:18:30 +0800

    feat: 复现时许问题

--- nutui-react_issue_3312/babel.config.js
+++ nutui-react_issue_3312/babel.config.js
@@ -6,17 +6,5 @@ module.exports = {
       framework: 'react',
       ts: true
     }]
-  ],
-  plugins: [
-    [
-      "import",
-      {
-        "libraryName": "@nutui/nutui-react",
-        "libraryDirectory": "dist/esm",
-        "style": 'css',
-        "camel2DashComponentName": false
-      },
-      'nutui-react-taro'
-    ]
   ]
 }
